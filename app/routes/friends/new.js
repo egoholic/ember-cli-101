bce8508878dd6f17b5e3fad12e13938a837d2e3a
Vladimir Melnik <egotraumatic@gmail.com>
2015-08-14 12:14:15 +0300

    feature: adds ability to delete friends

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -6,14 +6,14 @@ export default Ember.Route.extend({
   },
 
   actions: {
-  	save() {
-  	  console.log('+-- save action bubled up to friends new route');
-  	  return true;
-  	},
+    save() {
+      console.log('+-- save action bubled up to friends new route');
+      return true;
+    },
 
-  	cancel() {
-  	  console.log('+-- cancel action bubbled up to friend new route');
-  	  return true;
-  	}
+    cancel() {
+      console.log('+-- cancel action bubbled up to friend new route');
+      return true;
+    }
   }
 });
